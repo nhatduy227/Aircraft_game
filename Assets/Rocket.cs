e2884567dbd6fcc85c5a33122fde7fabd6dc842a
Nom <np656@drexel.edu>
2020-09-10 18:40:54 -0400

    Audio Added. Rocket model Added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource audiosource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    // Declares function 
+    void ProcessInput() 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (audiosource.isPlaying == false)
+            {
+                audiosource.Play();
+            }
+
+        }
+        else 
+        {
+            audiosource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
